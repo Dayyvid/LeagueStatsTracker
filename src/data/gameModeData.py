dd424f06bdffb0eee7926da1e4dfe891961f4280
David <davenguyen2@gmail.com>
2018-09-15 21:58:17 -0400

    Reformatted files for easier parsing and clarity

--- src/data/gameModeData.py
+++ src/data/gameModeData.py
@@ -1,10 +1,9 @@
-gameModeDict = {
+gameModeData = {
 	"450": 'Aram',
-	"1030": 'Odyssey',
-	"1040": 'Odyssey',
-	"1050": 'Odyssey',
-	"1060": 'Odyssey',
-	"1070": 'Odyssey',
-	"1080": 'Odyssey',
+	"1030": 'Odyssey Intro',
+	"1040": 'Odyssey Cadet',
+	"1050": 'Odyssey Crewmember',
+	"1060": 'Odyssey Captain',
+	"1070": 'Odyssey Onslaught',
 	"1200": 'Nexus Blitz'
 }
